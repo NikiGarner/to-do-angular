a64a0441eaa84a0711ee5455d7ead6996bd3788c
Maldon Meehan and Niki Garner <maldon+niki@>
2016-08-08 16:38:00 -0700

    add front end still having throw er; // Unhandled 'error' event

--- app/to-do-create-tasks.ts
+++ app/to-do-create-tasks.ts
@@ -3,31 +3,54 @@
 /// <reference path="to-do-listing-functions.ts" />
 
 var people = ToDoList.people;
-
 var tasks = [];
-tasks.push(new ToDoList.HomeTask("Do the dishes.", "High"));
-tasks.push(new ToDoList.HomeTask("Buy chocolate.", "Low", people.diane));
-tasks.push(new ToDoList.HomeTask("Wash the laundry.", "High"));
-tasks[0].markDone();
 
-tasks.push(new ToDoList.HobbyTask("Practice origami."));
-tasks.push(new ToDoList.HobbyTask("Bake a pie."));
+$(document).ready(function(){
+  var tasks = [];
+  $('.home-tasks').submit(function(event){
+    event.preventDefault();
+    console.log("made it this far");
+    var description = $("#task").val();
+    $("#task").val("");
+    var priority = $('#priority').val();
+    $('#priority').val("");
+    var assignedTo = $('#assignment').val();
+    $('#assignment').val("");
+    console.log(description);
+    console.log(priority);
+    tasks.push(new ToDoList.HomeTask(description, priority, assignedTo));
+    tasks.forEach(function(task) {
+      $('#list-tasks').append("<li> Home Task: " + task.description + " Priority: " + task.priority + " Assigned To:" + task.assignedTo + "</li>");
+    });
+  });
+});
 
-var today = new Date();
-var tomorrow = new Date();
-tomorrow.setDate(today.getDate() + 1);
-var nextDay = new Date();
-nextDay.setDate(today.getDate() + 2);
 
-tasks.push(new ToDoList.WorkTask(today, "Update blog.", "High", people.diane));
-tasks.push(new ToDoList.WorkTask(tomorrow, "Go to meeting.", "Medium", people.thor));
-tasks.push(new ToDoList.WorkTask(tomorrow, "Save the world.", "High", people.thor));
-tasks.push(new ToDoList.WorkTask(tomorrow, "Buy a really big hammer.", "Low", people.thor));
-tasks.push(new ToDoList.WorkTask(nextDay, "Clean ceiling.", "Low", people.loki));
 
-console.log(tasks);
-var thorTasks = ToDoList.describeTasksForPerson(people.thor, tasks);
-console.log("Here are Thor's tasks: ");
-for(var task of thorTasks){
-  console.log(task);
-}
+
+// tasks.push(new ToDoList.HomeTask("Do the dishes.", "High"));
+// tasks.push(new ToDoList.HomeTask("Buy chocolate.", "Low", people.diane));
+// tasks.push(new ToDoList.HomeTask("Wash the laundry.", "High"));
+// tasks[0].markDone();
+
+// tasks.push(new ToDoList.HobbyTask("Practice origami."));
+// tasks.push(new ToDoList.HobbyTask("Bake a pie."));
+//
+// var today = new Date();
+// var tomorrow = new Date();
+// tomorrow.setDate(today.getDate() + 1);
+// var nextDay = new Date();
+// nextDay.setDate(today.getDate() + 2);
+//
+// tasks.push(new ToDoList.WorkTask(today, "Update blog.", "High", people.diane));
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Go to meeting.", "Medium", people.thor));
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Save the world.", "High", people.thor));
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Buy a really big hammer.", "Low", people.thor));
+// tasks.push(new ToDoList.WorkTask(nextDay, "Clean ceiling.", "Low", people.loki));
+//
+// console.log(tasks);
+// var thorTasks = ToDoList.describeTasksForPerson(people.thor, tasks);
+// console.log("Here are Thor's tasks: ");
+// for(var task of thorTasks){
+//   console.log(task);
+// }
